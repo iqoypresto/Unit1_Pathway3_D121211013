02980d2328470c5f0320fdc5f25ee6924a57a044
iqoypresto <rfqimaulana888@gmail.com>
2023-10-18 18:30:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/projectbusinesscard/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.example.projectbusinesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.projectbusinesscard.ui.theme.ProjectBusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectBusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PersonalData(name: String, title: String, number: String, gh: String, email: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.android_logo)
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFDDECD6))
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(8.dp).fillMaxSize()
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .background(color = Color(0xFF2242534))
+            )
+            Text(
+                text = name,
+                fontSize = 34.sp,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .padding(top = 10.dp)
+            )
+            Text(
+                text = title,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFF2F6019),
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .padding(top = 10.dp)
+            )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 30.dp)
+        ) {
+            Column(
+                modifier = Modifier.wrapContentSize()
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = image,
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Text(
+                        text = number,
+                        fontSize = 13.sp,
+                        modifier = Modifier
+                            .padding(start = 20.dp)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = image,
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Text(
+                        text = gh,
+                        fontSize = 13.sp,
+                        modifier = Modifier
+                            .padding(start = 20.dp)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = image,
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Text(
+                        text = email,
+                        fontSize = 13.sp,
+                        modifier = Modifier
+                            .padding(start = 20.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+fun BusinessCardPreview() {
+    ProjectBusinessCardTheme {
+        PersonalData("Muh. Rifqi Maulana", "Android Developer Extraordinaire", "+62 81349606671", "@iqoypresto", "rfqimaulana888@gmail.com")
+    }
+}
